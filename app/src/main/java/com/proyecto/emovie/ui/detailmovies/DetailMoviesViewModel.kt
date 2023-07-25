44c37062788790581567c346f65efbca00509553
Erika Tovar <tatianatovar59@gmail.com>
2023-07-25 16:34:04 -0500

    add package and xml

--- app/src/main/java/com/proyecto/emovie/ui/detailmovies/DetailMoviesViewModel.kt
+++ app/src/main/java/com/proyecto/emovie/ui/detailmovies/DetailMoviesViewModel.kt
@@ -1,13 +1,13 @@
-package com.proyecto.emovie.ui.notifications
+package com.proyecto.emovie.ui.detailmovies
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class DetailMoviesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is detail Fragment"
     }
     val text: LiveData<String> = _text
 }
